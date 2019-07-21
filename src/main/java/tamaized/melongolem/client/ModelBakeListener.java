bb884ab3a06639d4690229fd54ee86f0ef470456
Tamaized <ryanolt123@gmail.com>
2019-07-20 23:57:48 -0400

    fixed block fullbright but not item fullbright #3

--- src/main/java/tamaized/melongolem/client/ModelBakeListener.java
+++ src/main/java/tamaized/melongolem/client/ModelBakeListener.java
@@ -27,7 +27,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Random;
 
-@Mod.EventBusSubscriber(modid = MelonMod.MODID, value = Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = MelonMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModelBakeListener {
 
 	@SubscribeEvent
