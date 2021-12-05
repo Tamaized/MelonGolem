c487eb66a6acf12ba17acddea66a5b27dd37a855
Andromander <cathy@feodoroff.com>
2021-12-05 12:48:58 +1000

    Renders

--- src/main/java/tamaized/melongolem/ClientProxy.java
+++ src/main/java/tamaized/melongolem/ClientProxy.java
@@ -1,24 +1,31 @@
 package tamaized.melongolem;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraft.client.renderer.entity.SpriteRenderer;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import tamaized.melongolem.client.GuiEditGolemSign;
 import tamaized.melongolem.client.RenderMelonGolem;
 
+@Mod.EventBusSubscriber(modid = MelonMod.MODID, value = Dist.CLIENT)
 public class ClientProxy implements IModProxy {
 
 //	public static final VertexFormat ITEM_FORMAT_WITH_LIGHTMAP = new VertexFormat(DefaultVertexFormats.ITEM).addElement(DefaultVertexFormats.TEX_2S);
 
 	public static void registerRenders() {
-		RenderingRegistry.registerEntityRenderingHandler(MelonMod.entityTypeMelonGolem, RenderMelonGolem.Factory::normal);
-		RenderingRegistry.registerEntityRenderingHandler(MelonMod.entityTypeMelonSlice, manager -> new SpriteRenderer<>(manager, Minecraft.getInstance().getItemRenderer()));
-		RenderingRegistry.registerEntityRenderingHandler(MelonMod.entityTypeTinyMelonGolem, RenderMelonGolem.Factory::tiny);
-		RenderingRegistry.registerEntityRenderingHandler(MelonMod.entityTypeGlisteringMelonGolem, RenderMelonGolem.Factory::glister);
+		ItemBlockRenderTypes.setRenderLayer(MelonMod.glisteringMelonBlock, RenderType.cutout());
+	}
 
-		RenderTypeLookup.setRenderLayer(MelonMod.glisteringMelonBlock, RenderType.getCutoutMipped());
+	@SubscribeEvent
+	public static void registerEntityRenders(EntityRenderersEvent.RegisterRenderers event) {
+		event.registerEntityRenderer(MelonMod.entityTypeMelonGolem, RenderMelonGolem.Factory::normal);
+		event.registerEntityRenderer(MelonMod.entityTypeMelonSlice, ThrownItemRenderer::new);
+		event.registerEntityRenderer(MelonMod.entityTypeTinyMelonGolem, RenderMelonGolem.Factory::tiny);
+		event.registerEntityRenderer(MelonMod.entityTypeGlisteringMelonGolem, RenderMelonGolem.Factory::glister);
 	}
 
 	@Override
@@ -34,7 +41,7 @@ public class ClientProxy implements IModProxy {
 	@Override
 	public void openSignHolderGui(ISignHolder golem) {
 		if (MelonMod.SIGNS.contains(golem.getHead().getItem()) && golem.getDistance(Minecraft.getInstance().player) <= 6)
-			Minecraft.getInstance().displayGuiScreen(new GuiEditGolemSign(golem));
+			Minecraft.getInstance().setScreen(new GuiEditGolemSign(golem));
 	}
 
 	/*public enum DefaultTextureGetter implements Function<ResourceLocation, TextureAtlasSprite> {
