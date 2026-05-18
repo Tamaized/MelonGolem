c089eb472a2ccec2e8ba8f045a917f9a73687139
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-05-18 09:19:05 -0400

    Remove boolean param from queue particle

--- src/main/java/tamaized/melongolem/common/PetHelper.java
+++ src/main/java/tamaized/melongolem/common/PetHelper.java
@@ -36,7 +36,7 @@ public class PetHelper {
 			ClientPacketSendParticles particles = new ClientPacketSendParticles();
 			for (int i = 0; i < 25; i++) {
 				Vec3 result = pet.getViewVector(1F).yRot(pet.getRandom().nextFloat() * 360F).xRot(pet.getRandom().nextFloat() * 360F).scale(0.35F);
-				particles.queueParticle(ParticleTypes.END_ROD, false, pet.getX() + result.x, pet.getY() + pet.getBbHeight() / 2F + result.y, pet.getZ() + result.z, 0, 0, 0);
+				particles.queueParticle(ParticleTypes.END_ROD, pet.getX() + result.x, pet.getY() + pet.getBbHeight() / 2F + result.y, pet.getZ() + result.z, 0, 0, 0);
 			}
 			PacketDistributor.sendToPlayersTrackingChunk(level, ChunkPos.containing(owner.blockPosition()), particles);
 			if (attachment.getPet().isEmpty())
